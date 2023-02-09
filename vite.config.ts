317a82e6a78553e56eacceb22df3a69e9c5f4871
青游 <2517789608@qq.com>
2023-02-09 23:22:58 +0800

    使用TSX代替Template

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// 浏览器渲染vue3-jsx语法需要插件：@vitejs/plugin-vue-jsx
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // base 是部署到github pages才用
   // base:'/bugu-fe/dist/',
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    vueJsx({
+      // github官方：理论上babel能配什么，它就能配什么
+      transformOn: true,
+      mergeProps: true,
+    }),
+  ]
 })
