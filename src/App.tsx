cf885027c780f9d17ecc7fd16ca0089397b7353f
青游 <2517789608@qq.com>
2023-02-10 00:25:16 +0800

    引入vue router 4.0.14

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { defineComponent, ref } from "vue";
+import { RouterView } from "vue-router";
 
 export const App = defineComponent({
   setup() {
@@ -7,12 +8,20 @@ export const App = defineComponent({
       refCount.value += 1
     }
     return () => <>
+      <header>导航
+        <ul>
+          <li>
+            <router-link to="/">Foo</router-link>
+          </li>
+          <li>
+            <router-link to="/about">Bar</router-link>
+          </li>
+        </ul> 
+      </header>
       <div>
-        {refCount.value}
+        <RouterView />
       </div>
-      <div>
-        <button onClick={onClick}>+1</button>
-      </div>
+      <footer>页脚</footer>
     </>
   }
 })
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
 import { App } from './App'
 
-createApp(App).mount('#app')
+import { Bar } from './views/Bar'
+import { Foo } from './views/Foo'
+import {createRouter, createWebHashHistory} from 'vue-router'
+
+const routes = [
+  { path: '/', component: Foo },
+  { path: '/about', component: Bar },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes, 
+})
+
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
