1588515da1d14ef0fc8680d6a761e868b28fd963
qingyou2517 <2517789608@qq.com>
2023-11-20 06:51:44 +0800

    feature: 制作第一个欢迎页面

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { defineComponent, ref } from "vue";
 import { RouterView } from "vue-router";
+import "./App.scss"
 
 export const App = defineComponent({
   setup() {
-    const refCount = ref(0)
-    const onClick = ()=>{
-      refCount.value += 1
-    }
     return () => (
       <div>
         <RouterView />
--- src/components/welcome/first.tsx
+++ src/components/welcome/first.tsx
@@ -1,9 +1,22 @@
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
+import s from "./First.module.scss";
+import pig from "../../assets/icons/pig.svg"
+import { RouterLink } from "vue-router";
 
 export const First = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div>First</div>
-    )
-  }
-})
+      <div class={s.wrapper}>
+        <div class={s.card}>
+          <img src={pig}  />
+          <h2>会挣钱<br/>也会省钱</h2>
+        </div>
+        <div class={s.actions}>
+          <span class={s.hide}>跳过</span>
+          <RouterLink to="/welcome/2"  class={s.next}>下一页</RouterLink>
+          <RouterLink to="/start">跳过</RouterLink>
+        </div>
+      </div>
+    );
+  },
+}); 
