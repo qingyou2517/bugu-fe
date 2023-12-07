58b9c45419b4418cff48e1c59157812b617d5466
qingyou2517 <2517789608@qq.com>
2023-12-07 19:14:24 +0800

    feature: 完成登录鉴权

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,31 @@ import { App } from './App'
 import { createRouter } from 'vue-router'
 import { routes } from './config/routes'
 import { history } from './shared/history';
+import { http } from "./shared/Http"
 
 import 'vant/lib/index.css';
 import "@svgstore";
 
 const router = createRouter({
   history,
-  routes, 
+  routes,
 })
+
+// 全局前置守卫
+router.beforeEach(async (to, from) => {
+  if (
+    to.path === "/" ||
+    to.path.startsWith("/welcome") ||
+    to.path.startsWith("/sign_in")
+  ) {
+    return true;
+  } else {
+    const jwt = localStorage.getItem("jwt")
+    const path = jwt ? true : "/sign_in?return_to=" + to.path
+    return path;
+  }
+});
+
 
 const app = createApp(App)
 app.use(router)
