eecdc9085c3c6e3ea448b5beaacea717063bd767
qingyou2517 <2517789608@qq.com>
2024-01-09 05:57:26 +0800

    feature: 提示敬请期待

--- src/shared/ComingSoon.tsx
+++ src/shared/ComingSoon.tsx
@@ -1,15 +1,45 @@
-import { defineComponent, PropType } from 'vue';
-import s from './ComingSoon.module.scss';
+import { defineComponent, PropType } from "vue";
+import s from "./ComingSoon.module.scss";
+import { Icon } from "./Icon";
+import { MainLayout } from "../layouts/MainLayout";
+import { useRoute, useRouter } from "vue-router";
 
 export const ComingSoon = defineComponent({
   props: {
     name: {
-      type: String as PropType<string>
-    }
+      type: String as PropType<string>,
+    },
   },
   setup: (props, context) => {
+    const router = useRouter();
+    const routes = useRoute();
+
+    type TitleMap = Record<string, string>;
+    const titleMap: TitleMap = {
+      "/export": "导出数据",
+      "/classify": "自定义分类",
+      "/notify": "记账提醒",
+    };
+
+    const goBack = () => {
+      router.push("/items");
+    };
+
     return () => (
-      <div class={s.wrapper}>正在开发中...</div>
-    )
-  }
-})
+      <MainLayout>
+        {{
+          title: () => `${titleMap[routes.fullPath]}`,
+          icon: () => <Icon name="home" class={s.navIcon} onClick={goBack} />,
+          default: () => (
+            <div class={s.wrapper}>
+              <div class={s.pig_wrapper}>
+                <Icon name="pig" class={s.pig} />
+              </div>
+              <p class={s.text}>开发中，敬请期待</p>
+            </div>
+          ),
+        }}
+      </MainLayout>
+    );
+  },
+});
